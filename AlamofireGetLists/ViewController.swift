d236a193fde4e629fd0dfa0698adfd1782b37246
Satyam Saluja <satyamsaluja@Satyams-MacBook-Air.local>
2017-06-22 01:52:10 +0530

    Initial Commit

---
+++ AlamofireGetLists/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlamofireGetLists
+//
+//  Created by Satyam Saluja on 22/06/17.
+//  Copyright © 2017 Satyam Saluja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
